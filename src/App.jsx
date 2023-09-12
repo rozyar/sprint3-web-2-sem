8c89a734985380c9ac96df26ff0f64ed1218ebfd
rozyar <rayzor@arboleya.me>
2023-09-12 20:28:51 -0300

    import created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { Outlet, useLocation } from 'react-router-dom'
 import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 
 function App() {
 
