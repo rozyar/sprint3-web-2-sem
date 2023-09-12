db1a7d789db9b644c3c20ca1be72e18e4e337ddb
rozyar <rayzor@arboleya.me>
2023-09-12 19:31:31 -0300

    required imports created on main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './routes/Home/Home.jsx'
+import PainelAcidente from './routes/PainelAcidente/PainelAcidente.jsx'
+import Error404 from './routes/Error/Error404.jsx'
+import VizualizarVeiculo from './routes/VizualizarVeiculo/VizualizarVeiculo.jsx'
+import VizualizarCategoria from './routes/VizualizarCategoria/VizualizarCategoria.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
