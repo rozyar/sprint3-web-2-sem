e53f35e49b087dfcdfd4b5cf0bcd6b9bef896d9d
K413l <kalel.schlichting@gmail.com>
2023-09-12 20:43:31 -0300

    jsx guincho criado

--- src/components/Carros/Guincho/Guincho.jsx
+++ src/components/Carros/Guincho/Guincho.jsx
@@ -1,13 +1,41 @@
-import 'react'
 import './Guincho.css';
-
+import tow from '../../../assets/tow-truck.jpg'
+import { useState } from "react";
 export default function Guincho(props) {
+    const [buttonText, setButtonText] = useState('Requisitar');
+    const [buttonColor, setButtonColor] = useState('');
+
   
+    const handleButtonClick = () => {
+        if (buttonText === 'Requisitar') {
+          setButtonText('Requisitado!');
+          setButtonColor('green');
+        } else {
+          setButtonText('Requisitar');
+          setButtonColor('');
+        }
+      };
       return (
           
           <>
-       
+       <div className="guincho-card">
+          <div className="guincho-card-image">
+              <img src={tow} alt={tow}/>
+          </div>
+          <div className="guincho-card-content">
+            <h3>Guincho do Alexandre</h3>
+            <button
+            className='guincho-submit'
+            onClick={(e) => {
+                e.preventDefault();
+                handleButtonClick();
+            }}
+            style={{backgroundColor: buttonColor}}>
+            {buttonText}
+          </button>
+          </div>
+       </div>
       
-          </>
+    </>
   );
 };
