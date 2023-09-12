e8aa73f3a38125ca08a96ade5d10684a00ba013e
K413l <kalel.schlichting@gmail.com>
2023-09-12 20:24:34 -0300

    cards carros criado

--- src/components/Carros/Carros.jsx
+++ src/components/Carros/Carros.jsx
@@ -1,3 +1,18 @@
 import { Link } from "react-router-dom";
 import './Carros.css';
 
+export default function Carros(props) {
+  return (
+
+    <>
+       <div className="carros-card">
+          <div className="carros-card-image">
+              <img src={props.img} alt={props.categoria}/>
+          </div>
+          <h3>{props.categoria}</h3>
+            <Link to={`/painelAcidente/${props.categoria}`}><input className='carros-submit' type="submit"  readOnly value='Selecionar'/></Link>
+       </div>
+      
+    </>
+  );
+}
