a0a36c45af6950d2fe3a797374d9722197ab2fa4
rozyar <rayzor@arboleya.me>
2023-09-12 22:43:28 -0300

    imports created

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,5 +1,7 @@
 import 'react'
+import Login from '../../components/Login/Login'
 import './Home.css'
+import BackgroundLogin from '../../components/Login/BackgroundLogin/BackgroundLogin'
 
 
 export default function Home() {
