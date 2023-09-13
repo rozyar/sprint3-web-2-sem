5fa94df55a6c723ab3a303f364e86a399d4ddbaf
Dericki <derick.araujoo@gmail.com>
2023-09-12 22:13:28 -0300

    adicionando p form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -17,6 +17,14 @@ export default function Login() {
                             <h1>Bem vindo a Porto Seguro</h1>
                             <p>Ainda não possui uma conta? </p>
                         </div>
+                        <div className='login-form-input'>
+                            <input className='login-user-input' type="text" name='cpf' value={cpf} onChange={(e) =>{e.preventDefault; setCpf(e.target.value)}} maxLength={11} placeholder='Digite seu cpf'/>
+                            <input className='login-user-input' type="password" name='cpf' value={password} onChange={(e) => {e.preventDefault; setPassword(e.target.value)}} maxLength={11} placeholder='Digite sua senha'/>
+                            <Link to={'/painelAcidente'}><input className='login-user-input-submit' type="submit" value='Login'/></Link>
+                        </div>
+                        <div className="login-form-terms">
+                        <p>Ao se inscrever, você concorda com nossos <u>Termos</u> e <u>Política de Privacidade</u>.</p>
+                        </div>
                     </div>
                 </div>
 
