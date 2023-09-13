f667ceb4892f7b9e3e6b8ec4fa027af9b4c7f21f
Dericki <derick.araujoo@gmail.com>
2023-09-12 22:21:12 -0300

    adcionando conteudo no background

--- src/components/Login/BackgroundLogin/BackgroundLogin.jsx
+++ src/components/Login/BackgroundLogin/BackgroundLogin.jsx
@@ -4,7 +4,18 @@ import './BackgroundLogin.css'
 export default function BackgroundLogin(){
     return (
         <>
+            <div className='background-login-image'>
+                
+                <div className='background-login-blur'>
+                    
+                    <div className='background-login-text'>
+                        <h2>2.500 Empresas e pessoas como você já confiaram em nossa companhia de seguros.</h2>
+                        <h1><strong>Porto Seguro</strong>, sempre ao seu lado.</h1>
+                    </div>
+                
+                </div>
             
+            </div>
         </>
     )
 }
