e0931804eac065f6c394a606d41be3d92ccd382e
PhabloFiap <rm550687@fiap.com.br>
2023-09-12 21:50:45 -0300

    Começando a fazer imports

--- src/routes/VizualizarCategoria/VizualizarCategoria.jsx
+++ src/routes/VizualizarCategoria/VizualizarCategoria.jsx
@@ -1,4 +1,8 @@
 import 'react'
+import arrow from '../../assets/Arrow.png'
+import { Link, useParams } from 'react-router-dom'
+import { ListCarros as list} from '../../components/Carros/ListCarros'
+import CarrosDetalhados from '../../components/Carros/CarrosDetalhados/CarrosDetalhados'
 import './VizualizarCategoria.css'
 
 export default function VizualizarCategoria() {
