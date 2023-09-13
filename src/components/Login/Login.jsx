94b29ed3b417a6277d0cb279c390deb5c2a99451
Dericki <derick.araujoo@gmail.com>
2023-09-12 22:04:52 -0300

    adicionando imagem

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,7 +5,13 @@ export default function Login() {
     
     return (
         <>
+
+            <div className='login-container'>
+                <div className='login-logo'>
+                    <img src={Porto} alt="Porto Logo" width={138} height={32} />
+                </div>
             
+            </div> 
         </>
     )
 }
