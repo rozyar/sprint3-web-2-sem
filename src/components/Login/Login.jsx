14ace0a0e69ed266108a7642d57bc5b16a781b57
Dericki <derick.araujoo@gmail.com>
2023-09-12 22:10:48 -0300

    adicionando h1/p

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,13 +5,15 @@ export default function Login() {
     
     return (
         <>
-
             <div className='login-container'>
-                <div className='login-logo'>
-                    <img src={Porto} alt="Porto Logo" width={138} height={32} />
+
+                <div>
+                    <div>
+                        <img />
+                    </div>
                 </div>
-            
-            </div> 
+
+            </div>
         </>
     )
 }
