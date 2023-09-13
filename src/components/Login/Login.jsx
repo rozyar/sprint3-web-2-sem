163923521eaa44dd434f47ace6c5b734916ee4b5
Dericki <derick.araujoo@gmail.com>
2023-09-12 22:11:08 -0300

    adicionando conteudo

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,9 +7,16 @@ export default function Login() {
         <>
             <div className='login-container'>
 
-                <div>
-                    <div>
-                        <img />
+                <div className='login-area'>>
+                    <div className='login-logo'>
+                        <img src={Porto} alt="Porto Logo" width={138} height={32} />
+                    </div>
+
+                    <div className='login-form'>
+                        <div className='login-form-title'>
+                            <h1>Bem vindo a Porto Seguro</h1>
+                            <p>Ainda não possui uma conta? </p>
+                        </div>
                     </div>
                 </div>
 
