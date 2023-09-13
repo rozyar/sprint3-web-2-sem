f9a661151de200492a19f726ea838ac49b295244
Razyel420 <razyel.arboleya@gmail.com>
2023-09-12 22:13:15 -0300

    div header container criada

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,18 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return (
         <>
+            <div className="header-container"> 
+                
+                <div className="header-content">
+                    <img src={Porto} alt="cart" width={60} height={60}/>
+                    <h1>Painel de Acidente</h1>
+                </div>
+            
+                
+                
+            </div>
+            
+            
     
         </>
     )
