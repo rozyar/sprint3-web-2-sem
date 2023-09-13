36503457b9ca7f2941bd0aeb7bc8398538e067b7
PhabloFiap <rm550687@fiap.com.br>
2023-09-12 21:39:03 -0300

    Importando arquivos

--- src/routes/PainelAcidente/PainelAcidente.jsx
+++ src/routes/PainelAcidente/PainelAcidente.jsx
@@ -1,7 +1,12 @@
 import 'react'
+import Carros from '../../components/Carros/Carros'
 import './PainelAcidente.css'
+import arrow from '../../assets/Arrow.png'
+import  {ListCarros}  from '../../components/Carros/ListCarros'
+import { Link } from 'react-router-dom'
 
 export default function PainelAcidente() {
+    
     return (
         <>
             
