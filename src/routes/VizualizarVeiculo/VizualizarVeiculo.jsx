710e3f335d3f5dbcb25cf5988d09f3d701481bc3
rozyar <rayzor@arboleya.me>
2023-09-12 21:14:49 -0300

    import created

--- src/routes/VizualizarVeiculo/VizualizarVeiculo.jsx
+++ src/routes/VizualizarVeiculo/VizualizarVeiculo.jsx
@@ -1,5 +1,10 @@
 import 'react'
+import arrow from '../../assets/Arrow.png'
+import { Link, useParams } from 'react-router-dom'
+import { ListCarros  as list} from '../../components/Carros/ListCarros'
 import './VizualizarVeiculo.css'
+import Guincho from '../../components/Carros/Guincho/Guincho'
+import CarrosDetalhados from '../../components/Carros/CarrosDetalhados/CarrosDetalhados'
 
 export default function VizualizarVeiculo() {
     
